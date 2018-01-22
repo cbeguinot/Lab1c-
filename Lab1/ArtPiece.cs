f2349d6e9d39d70fb332948c7c643200be5a88da
Louis Mahl <mahl@edu.ece.fr>
2018-01-22 15:41:09 +0100

    Art pieces

--- Lab1/ArtPiece.cs
+++ Lab1/ArtPiece.cs
@@ -7,12 +7,37 @@ using System.Threading.Tasks;
 namespace Lab1
 {
     enum Categories {PAINTING, DRAWING, SCULPTURE};
-    class ArtPiece
+    class ArtPiece: IComparable
     {
         private String m_title;
         private Categories m_category;
         private int m_price;
 
+        public String title { get; set; }
+        public Categories category { get; set; }
+        public int price { get; set; }
 
+        ArtPiece(String title, Categories category, int price)
+        {
+            m_title = title;
+            m_category = category;
+            m_price = price;
+        }
+
+        public void CompareTo(Object o)
+        {
+
+        }
+
+        override
+        public String ToString()
+        {
+            return "Ceci est une piece artistique !";
+        }
+
+        int IComparable.CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
