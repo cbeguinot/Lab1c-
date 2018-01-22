11ef1c1731924b306b665fd3143b45ab5cf48078
Louis Mahl <mahl@edu.ece.fr>
2018-01-22 16:11:07 +0100

    suite

--- Lab1/ArtPiece.cs
+++ Lab1/ArtPiece.cs
@@ -25,10 +25,7 @@ namespace Lab1
             m_price = price;
         }
 
-        public void CompareTo(Object o)
-        {
-
-        }
+        public void CompareTo(object o) { return this.price.CompareTo((o as ArtPiece).price); }
 
         override
         public String ToString()
