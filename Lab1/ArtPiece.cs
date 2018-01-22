c686867e3ed1ad30c7ec9b04028f4a6584d1f717
Caroline Béguinot <caroline.beguinot@edu.ece.fr>
2018-01-22 15:53:24 +0100

    plus cassé

--- Lab1/ArtPiece.cs
+++ Lab1/ArtPiece.cs
@@ -7,12 +7,8 @@ using System.Threading.Tasks;
 namespace Lab1
 {
     enum Categories {PAINTING, DRAWING, SCULPTURE};
-<<<<<<< HEAD
-    class ArtPiece: IComparable
-=======
 
-    class ArtPiece
->>>>>>> 458e8c1e09ab1241f76fc6b65bccaffdc8a68a71
+    class ArtPiece: IComparable
     {
         private String m_title;
         private Categories m_category;
