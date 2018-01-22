d2ab4266bf260b731c212c62ff62730fe52fe618
Caroline Béguinot <caroline.beguinot@edu.ece.fr>
2018-01-22 14:46:55 +0100

    svp marche

--- Lab1/ArtPiece.cs
+++ Lab1/ArtPiece.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Lab1
 {
+    enum Categories {PAINTING, DRAWING, SCULPTURE};
     class ArtPiece
     {
+        private String m_title;
+        private Categories m_category;
+        private int m_price;
+
+
     }
 }
